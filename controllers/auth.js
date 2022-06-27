45b591c198ef122f9395de318245eb4c36e9deae
Luis Morales <dev.luismorales@gmail.com>
2022-06-27 12:06:25 -0500

    se guardo un registro en la base de datos

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,50 @@
 const { response } =  require('express');
-const {validationResult} = require("express-validator");
+const Usuario = require('../models/Usuario')
 
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
     const { name, email, password } = req.body;
 
-    return res.json({
-        ok: true,
-        message: 'Crear usuario /new'
-    })
+    try {
+        // Verificar email
+        let usuario = await Usuario.findOne({email});
+        if ( usuario) {
+            return res.status(400).json({
+                ok: false,
+                message: 'El usuario ya existe con ese email'
+            });
+        }
+
+        // Crear usuario de el modelo
+        const dbUsuario = new Usuario( req.body );
+
+        // Encripatar contraseñas con hash
+
+        // Generar JWT
+
+        // Crear usuario de DB
+        await dbUsuario.save()
+        // Generar respuesta exitosa
+        res.status(201).json({
+            ok: true,
+            uid: dbUsuario.id,
+            name
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            message: 'Por favor hable con el administrador'
+        })
+
+    }
+
 
 }
 const loginUsuario = (req, res = response) => {
-
-    const errors = validationResult( req );
 
     const { email, password } = req.body;
     console.log(email, password);
